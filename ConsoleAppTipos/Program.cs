76bed96e9c74be5cb21666d6ead593e09a9a338a
marcelogdiaz <marcelogdiaz@gmail.com>
2026-04-14 11:47:10 -0300

    Commit inicial

---
+++ ConsoleAppTipos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTipos
+{
+    //consoleapptipos
+
+    /// <summary>
+    /// Esta es clase principal de nuestro proyecto
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Este es el metodo principal de nuestro proyecto
+        /// </summary>
+        /// <param name="args">este es un parametro de entrada de datos</param>
+        static void Main(string[] args)
+        {
+            //Single s = 100;
+
+            //if (s < Single.MinValue)
+            //{
+            //    Console.WriteLine("Your number is smaller than a Single.MINVALUE.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your number is greater than a Single.MINVALUE");
+            //}
+
+            double myDouble = 10.5;
+
+            Console.WriteLine(myDouble);
+            float myfloat = 10.5F;  //explicito
+            //implicito
+            Console.WriteLine(myfloat);
+
+            //---------------------
+            int myInt = 8;
+            Console.WriteLine("Antes del CAST");
+            Console.WriteLine(myInt);
+
+            myInt = (int)myfloat; //conversion explicita mediante CAST
+
+            Console.WriteLine("Despues del CAST");
+            Console.WriteLine(myInt);
+        }
+    }
+}
---
+++ ConsoleAppTipos2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTipos2
+{
+    class Program
+    {
+        /// <summary>
+        /// Tipo de dato ESTRUCTURADO, tipo por valor
+        /// </summary>
+        struct Book
+        {
+            public string title;
+            public string author;
+            public string subject;
+            public int book_id;
+        }
+
+        /// <summary>
+        /// Tip de dato ENUMERADO, tipo por valor
+        /// </summary>
+        enum Dias { LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO }
+
+        static void Main(string[] args)
+        {
+            Book miLibro = new Book
+            {
+                title = "Sistemas Operativos",
+                author = "A. Silberchatz",
+                subject = "Computacion",
+                book_id = 1
+            };
+
+
+            Book miLibro2 = new Book();
+
+            
+
+            miLibro2 = miLibro;
+            miLibro2.title = ".NET";
+
+
+            Console.WriteLine(miLibro.title);
+            Console.WriteLine(miLibro);
+
+            Console.WriteLine("Hoy es " + Dias.MARTES);
+            Console.ReadLine();
+
+        }
+    }
+}
