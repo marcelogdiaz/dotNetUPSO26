b9a7bf9db931fb4415bf128fb61e9f13b258c391
marcelogdiaz <marcelogdiaz@gmail.com>
2026-04-21 20:35:25 -0300

    Clase 5

--- S4_NUMEROS/Program.cs
+++ S4_NUMEROS/Program.cs
@@ -4,38 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace S4_NUMEROS
+namespace S4_S5_NUMEROS
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //RANGO DE VALORES
-            Console.WriteLine("RANGOS DE ENTEROS");
-            Console.WriteLine($"short.MinValue = {short.MinValue}");
-            Console.WriteLine($"short.MaxValue = {short.MaxValue}");
-            Console.WriteLine($"int.MinValue = {int.MinValue}");
-            Console.WriteLine($"int.MaxValue = {int.MaxValue}");            
-            Console.WriteLine($"long.MinValue = {long.MinValue}");
-            Console.WriteLine($"long.MaxValue = {long.MaxValue}");
+            ////RANGO DE VALORES
+            //Console.WriteLine("RANGOS DE ENTEROS");
+            //Console.WriteLine($"short.MinValue = {short.MinValue}");
+            //Console.WriteLine($"short.MaxValue = {short.MaxValue}");
+            //Console.WriteLine($"int.MinValue = {int.MinValue}");
+            //Console.WriteLine($"int.MaxValue = {int.MaxValue}");
+            //Console.WriteLine($"long.MinValue = {long.MinValue}");
+            //Console.WriteLine($"long.MaxValue = {long.MaxValue}");
 
-            Console.WriteLine("------------------------");
-            Console.WriteLine("RANGOS DE REALES");
-            Console.WriteLine($"float.MinValue = {float.MinValue}");
-            Console.WriteLine($"float.MaxValue = {float.MaxValue}");
-            Console.WriteLine($"double.MinValue = {double.MinValue}");
-            Console.WriteLine($"double.MaxValue = {double.MaxValue}");
-            Console.WriteLine($"decimal.MinValue = {decimal.MinValue}");
-            Console.WriteLine($"decimal.MaxValue = {decimal.MaxValue}");
+            //Console.WriteLine("------------------------");
+            //Console.WriteLine("RANGOS DE REALES");
+            //Console.WriteLine($"float.MinValue = {float.MinValue}");
+            //Console.WriteLine($"float.MaxValue = {float.MaxValue}");
+            //Console.WriteLine($"double.MinValue = {double.MinValue}");
+            //Console.WriteLine($"double.MaxValue = {double.MaxValue}");
+            //Console.WriteLine($"decimal.MinValue = {decimal.MinValue}");
+            //Console.WriteLine($"decimal.MaxValue = {decimal.MaxValue}");
 
-            //DECLARACION DE LITERALES
-            double numDouble = 3.78;
-            decimal numDecimal = 3.78M;
-            float numFloat = 3.78F;
+            ////DECLARACION DE LITERALES
+            //double numDouble = 3.78;
+            //decimal numDecimal = 3.78M;
+            //float numFloat = 3.78F;
 
-            int numInt;
-            numInt = (int)numDouble;
-            numInt = (int)numDecimal;
+            //int numInt;
+            //numInt = (int)numDouble;
+            //numInt = (int)numDecimal;
+
+            int i = -3;
+            //Console.WriteLine("Valor Orignal de i: " + i);
+            //Console.WriteLine("++i retorna: " + ++i);
+            //Console.WriteLine("i++ retorna: " + i++);
+
+            double d = 5.5;
+            Console.WriteLine("Valor Orignal de d: " + d);
+            Console.WriteLine("--d retorna: " + --d);
+            Console.WriteLine("d-- retorna: " + d--);
+            Console.WriteLine("Valor -d: " + -d);
+            Console.WriteLine("Valor final de d: " + d);
+
+            Console.WriteLine(+(i));
+            Console.WriteLine(-4);
+
+            Console.WriteLine(-(-4));
 
             Console.ReadLine();
         }
---
+++ S5_STRING/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S5_STRING
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string a = "hello";
+            //string b = "h";
+            ////string c;
+
+
+            //b += "ello";
+
+            //a = b;
+
+            //Console.WriteLine(a==b);
+            //Console.WriteLine(object.ReferenceEquals(a,b));
+            //Console.ReadLine();
+
+            string parrafo = "esto es un " +
+                "" +
+                "" +
+                "parrafo";
+
+            string texto = @" esto es un parrafo
+                    que tiene varias
+                    lineas
+                    ";
+
+            //Console.WriteLine(parrafo);
+            //Console.WriteLine("---------------");
+
+            //Console.WriteLine(texto);
+
+            string frase = parrafo.Substring(5);
+            string minifrase = parrafo.Substring(5,3);
+            Console.WriteLine(frase);
+            Console.WriteLine(minifrase);
+
+            string frase1 = "Her name was \"Sara\".";
+            string frase2 = @" Her name was ""Sara."" ";
+
+            Console.WriteLine(frase1);
+            Console.WriteLine(frase2);
+
+            Console.ReadLine();
+        }
+    }
+}
