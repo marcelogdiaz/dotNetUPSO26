41fe0a7a7d739b8a0d6d3203a655c75c10b80721
marcelogdiaz <marcelogdiaz@gmail.com>
2026-05-26 17:45:55 -0300

    Ejemplo de Clases

---
+++ S8_CLASES/Entidades/Persona.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S8_CLASES.Entidades
+{
+    class Persona
+    {
+        #region PROPERTIES
+        /// <summary>
+        /// Campo para almacenar el nombre de una Persona
+        /// </summary>
+        private string nombre;
+        private string apellido;
+        private int edad;
+
+        public string Nombre { get; set; }  //version reducida de property
+        public string Apellido { get => apellido; set => apellido = value.ToUpper(); }  //version personalizada de property   gonzalez --> GONZALEZ
+        public int Edad { get => edad; set => edad = value; }   //version ampliada de property
+        #endregion
+
+        /// <summary>
+        /// Metodo para crear un objeto de la clase Persona
+        /// </summary>
+        /// <param name="n">Permite especificar el nombre de una persona</param>
+        /// <param name="a">Permite especificar el apellido de una persona</param>
+        /// <param name="e">Permite especificar la edad de una persona</param>
+        public Persona(string n, string a, int e)
+        {
+            Nombre = n;
+            Apellido = a;
+            Edad = e;
+
+            //a = Apellido;
+        }
+
+        /// <summary>
+        /// Metodo que incrementa la edad actual en 1
+        /// </summary>
+        /// <returns>Retorna la edad cumplida</returns>
+        public int CumplirAnios() {
+            Edad = Edad + 1;
+            return Edad;
+        }
+    }
+}
---
+++ S8_CLASES/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using S8_CLASES.Entidades;
+
+namespace S8_CLASES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string ap = "Perez";
+
+            //Persona p = new Persona("Juan",  ap, 33); //Persona p = new Persona("Juan", ref ap,33);
+
+            //Console.WriteLine("Edad antes del cumple...");
+            //Console.WriteLine("{0}, {1}, {2}", p.Apellido,p.Nombre, p.Edad);
+
+            //p.CumplirAnios();
+
+            //Console.WriteLine("Edad despues del cumple...");
+            //Console.WriteLine("{0}, {1}, {2}", p.Apellido, p.Nombre, p.Edad);
+            //Console.ReadLine();
+
+
+            //creamos un jugador en particular
+            Jugador j10 = new Jugador("Lionel", "Messi", 36, "delantero");
+
+            j10.CumplirAnios();
+
+            j10.Correr();
+            Console.ReadLine();
+        }
+    }
+}
