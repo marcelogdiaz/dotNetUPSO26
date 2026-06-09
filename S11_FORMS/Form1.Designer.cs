42ee2ad6dda11b700809d9f79abe6af11801d020
marcelogdiaz <marcelogdiaz@gmail.com>
2026-06-09 17:22:07 -0300

    Formularios y controles

---
+++ S11_FORMS/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace S11_FORMS
+{
+    partial class FormPpal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonCambiarTitulo = new System.Windows.Forms.Button();
+            this.labelBoton = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.labelCheckbox = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBoxTitulo = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Edad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonCambiarTitulo
+            // 
+            this.buttonCambiarTitulo.Location = new System.Drawing.Point(19, 46);
+            this.buttonCambiarTitulo.Name = "buttonCambiarTitulo";
+            this.buttonCambiarTitulo.Size = new System.Drawing.Size(158, 47);
+            this.buttonCambiarTitulo.TabIndex = 0;
+            this.buttonCambiarTitulo.Text = "Cambiar titulo";
+            this.buttonCambiarTitulo.UseVisualStyleBackColor = true;
+            this.buttonCambiarTitulo.Click += new System.EventHandler(this.buttonCambiarTitulo_Click);
+            this.buttonCambiarTitulo.MouseEnter += new System.EventHandler(this.buttonCambiarTitulo_Enter);
+            this.buttonCambiarTitulo.MouseLeave += new System.EventHandler(this.buttonCambiarTitulo_Leave);
+            this.buttonCambiarTitulo.MouseHover += new System.EventHandler(this.buttonCambiarTitulo_MouseHover);
+            // 
+            // labelBoton
+            // 
+            this.labelBoton.AutoSize = true;
+            this.labelBoton.Location = new System.Drawing.Point(215, 63);
+            this.labelBoton.Name = "labelBoton";
+            this.labelBoton.Size = new System.Drawing.Size(16, 13);
+            this.labelBoton.TabIndex = 1;
+            this.labelBoton.Text = "...";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(31, 203);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Tildar Opción";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // labelCheckbox
+            // 
+            this.labelCheckbox.AutoSize = true;
+            this.labelCheckbox.Location = new System.Drawing.Point(218, 204);
+            this.labelCheckbox.Name = "labelCheckbox";
+            this.labelCheckbox.Size = new System.Drawing.Size(82, 13);
+            this.labelCheckbox.TabIndex = 3;
+            this.labelCheckbox.Text = "Valor Checkbox";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Opcion 1",
+            "Opcion 2",
+            "Opcion 3"});
+            this.comboBox1.Location = new System.Drawing.Point(31, 250);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(50, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "cat A";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(31, 300);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(126, 115);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Categorias";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(15, 77);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(50, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "cat C";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(15, 53);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(50, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "cat B";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxTitulo
+            // 
+            this.textBoxTitulo.Location = new System.Drawing.Point(35, 16);
+            this.textBoxTitulo.Name = "textBoxTitulo";
+            this.textBoxTitulo.Size = new System.Drawing.Size(142, 20);
+            this.textBoxTitulo.TabIndex = 7;
+            this.textBoxTitulo.Text = "Ingrese un titulo...";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonCambiarTitulo);
+            this.panel1.Controls.Add(this.textBoxTitulo);
+            this.panel1.Controls.Add(this.labelBoton);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(353, 153);
+            this.panel1.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Edad,
+            this.Nombre,
+            this.Apellido});
+            this.dataGridView1.Location = new System.Drawing.Point(412, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(310, 174);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Edad
+            // 
+            this.Edad.HeaderText = "Edad";
+            this.Edad.Name = "Edad";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Apellido
+            // 
+            this.Apellido.HeaderText = "Apellido";
+            this.Apellido.Name = "Apellido";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(439, 268);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // FormPpal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(758, 440);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelCheckbox);
+            this.Controls.Add(this.checkBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormPpal";
+            this.Text = "Formulario Principal";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCambiarTitulo;
+        private System.Windows.Forms.Label labelBoton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label labelCheckbox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBoxTitulo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Edad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellido;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ S11_FORMS/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S11_FORMS
+{
+    //Este es el archivo editable de nuestro FORMULARIO
+
+    public partial class FormPpal : Form
+    {
+        int cantidadClicks = 1;
+        public FormPpal()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCambiarTitulo_Click(object sender, EventArgs e)
+        {
+            this.Text = textBoxTitulo.Text;
+            //cantidadClicks++;
+        }
+
+        private void buttonCambiarTitulo_Enter(object sender, EventArgs e)
+        {
+            labelBoton.Text = "Ingresé al foco del BOTON";
+        }
+
+        private void buttonCambiarTitulo_Leave(object sender, EventArgs e)
+        {
+            labelBoton.Text = "Salí del foco del BOTON";
+        }
+
+        private void buttonCambiarTitulo_MouseHover(object sender, EventArgs e)
+        {
+            labelBoton.Text = "Hover dentro del BOTON";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked ) {
+                labelCheckbox.Text = "El checkbox esta tildado";
+            }
+            else {
+                labelCheckbox.Text = "El checkbox NO esta tildado";
+            } 
+        }
+    }
+}
